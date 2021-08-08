da313232f24f562616bf01ca79c38418659bfcfc
William Prudencio <36481731+wprudencio97@users.noreply.github.com>
2021-08-08 18:35:19 -0400

    initial heroku deployment

--- password_generator/settings.py
+++ password_generator/settings.py
@@ -13,6 +13,8 @@ import os
 
 from pathlib import Path
 
+import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -131,3 +133,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
